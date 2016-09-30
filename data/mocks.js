fc532e5b42037fe555fbb43ac2d2955278d33902
tomatagolo <tom@agolo.com>
2016-09-30 14:54:08 -0400

    add basic schema for Agolo

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,41 @@
+import casual from 'casual';
+import cuid from 'cuid';
+
 const mocks = {
   String: () => 'It works!',
+  Query: () => ({
+    author: (root, args) => {
+      return { firstName: casual.first_name, lastName: casual.last_name };
+    }
+  }),
+  User: () => ({
+    email: casual.email,
+    _id: cuid(),
+    roles: casual.array_of_words(3)
+  }),
+  Author: () => ({ firstName: casual.first_name, lastName: casual.last_name }),
+  Post: () => ({
+    title: casual.title,
+    text: casual.sentences(3)
+  }),
+  Feed: () => ({
+    name: casual.word,
+    _id: cuid(),
+    recentClusterCount: casual.integer(0, 1000),
+    unseenClusterCount: casual.integer(0, 500),
+  }),
+  Cluster: () => ({
+    _id: cuid(),
+    title: casual.title
+  }),
+  SummarySection: () => ({
+    sentences: casual.sentences(2).split('.')
+  }),
+  SummaryMetadata: () => ({
+    published_at: casual.date(),
+    source: casual.word,
+    url: casual.url
+  })
 };
 
 export default mocks;
--- data/schema.js
+++ data/schema.js
@@ -1,6 +1,123 @@
 const typeDefinitions = `
+type Author {
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  title: String
+  text: String
+  author: Author
+}
+
+type User {
+  email: String
+  _id: String
+  roles: [String]
+  stripe: Stripe
+  feeds: [Feed]
+}
+
+type Stripe {
+  card: Card
+  customerId: String
+  payments: [Payment]
+  token: String
+  subscription: Subscription
+}
+
+type Payment {
+  amount: Int
+  date: String
+}
+
+type Subscription {
+  created: Int
+  canceled_at: Int
+  current_period_end: Int
+  current_period_start: Int
+  customer: String
+  id: String
+  plan: Plan
+  status: String
+  start: Int
+  trial_end: Int
+  trial_start: Int
+}
+
+type Card {
+  brand: String
+  country: String
+  cvc_check: String
+  exp_month: Int
+  exp_year: Int
+  funding: String
+  id: String
+  last4: String
+}
+
+type Plan {
+  amount: Int
+  created: Int
+  id: String
+  interval: String
+  name: String
+  trial_period_days: Int
+}
+
+type Feed {
+  _id: String
+  name: String
+  recentClusterCount: Int
+  unseenClusterCount: Int
+  users: [User]
+  recentClusters: [Cluster]
+  hotClusters: [Cluster]
+}
+
+type Cluster {
+  _id: String
+  title: String
+  summary: Summary
+  articles: [Article]
+  source: [Source]
+}
+
+type Summary {
+  summary: [SummarySection]
+}
+
+type SummarySection {
+  sentences: [String]
+  metadata: SummaryMetadata
+}
+
+type SummaryMetadata {
+  id: String
+  published_at: String
+  source: String
+  title: String
+  url: String
+}
+
+type Article {
+  title: String
+  text: String
+}
+
+type Source {
+  _id: String
+  format: String
+  name: String
+  publisher: String
+}
+
+
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  user(_id: String, email: String): User
+  userFeeds(_id: String, email: String): [Feed]
 }
 
 schema {
